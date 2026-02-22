a1fe1c0d0f7886710d433da4b7abd25ed5afef05
horothesun <horothesun@gmail.com>
2026-02-22 22:26:47 +0000

    Stack resolver 23.28 + GHC 9.8.4 (solving HLS issues)

--- setup_haskell.sh
+++ setup_haskell.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# - LATEST stable Stack resolver (from www.stackage.org): "LTS 24.25"
+# - LATEST stable Stack resolver (from www.stackage.org): "LTS 23.28"
 # - hls-powered check from `ghcup tui`
-GHC_LTS_24_25_AND_HLS_POWERED_VERSION="9.10.3"
+GHC_LTS_23_28_AND_HLS_POWERED_VERSION="9.8.4"
 
 CABAL_LATEST_VERSION="3.16.0.0"
 
 STACK_LATEST_VERSION="3.7.1"
 
-HLS_LATEST_VERSION="2.12.0.0"
+HLS_LATEST_VERSION="2.10.0.0"
 
-ghcup install ghc "${GHC_LTS_24_25_AND_HLS_POWERED_VERSION}"
-ghcup set ghc "${GHC_LTS_24_25_AND_HLS_POWERED_VERSION}"
+ghcup install ghc "${GHC_LTS_23_28_AND_HLS_POWERED_VERSION}"
+ghcup set ghc "${GHC_LTS_23_28_AND_HLS_POWERED_VERSION}"
 
 ghcup install cabal "${CABAL_LATEST_VERSION}"
 ghcup set cabal "${CABAL_LATEST_VERSION}"
@@ -19,5 +19,5 @@ ghcup set cabal "${CABAL_LATEST_VERSION}"
 ghcup install stack "${STACK_LATEST_VERSION}"
 ghcup set stack "${STACK_LATEST_VERSION}"
 
-ghcup compile hls --git-ref "${HLS_LATEST_VERSION}" --ghc "${GHC_LTS_24_25_AND_HLS_POWERED_VERSION}" --cabal-update
+ghcup compile hls --git-ref "${HLS_LATEST_VERSION}" --ghc "${GHC_LTS_23_28_AND_HLS_POWERED_VERSION}" --cabal-update
 ghcup set hls "${HLS_LATEST_VERSION}"
